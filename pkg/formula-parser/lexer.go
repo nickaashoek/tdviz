79c3b794466945745e5afa42169cac7444850416
nickaashoek <nick.kaashoek@gmail.com>
2021-09-30 18:47:24 -0400

    More setup

---
+++ pkg/formula-parser/lexer.go
@@ -0,0 +1,8 @@
+package formulaparser
+
+import "github.com/timtadh/lexmachine"
+
+// SetupLexer should setup the lexer for handling transition formulas
+func SetupLexer() *lexmachine.Scanner {
+	return &lexmachine.Scanner{}
+}
